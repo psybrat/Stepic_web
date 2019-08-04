00a09ad19fc7e906d90e7452f259ae88ed0ae6d7
psybrat <georgyvik@gmail.com>
2019-08-04 17:35:47 +0300

    gunicorn config + hello.py added lesson 1.9

---
+++ hello.py
@@ -0,0 +1,8 @@
+
+
+def echo(env, start_response):
+    result = '\r\n'.join(env['QUERY_STRING'].split('&'))
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    start_response(status, headers)
+    return result
--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/wsgi.conf /etc/gunicorn.d/wsgi-test
+sudo /etc/init.d/gunicorn restart
