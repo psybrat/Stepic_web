c911c7194c18592d4692db830c63502689c458e9
psybrat <georgyvik@gmail.com>
2019-08-17 18:55:43 +0300

    Первый коммит в новой ветке Second-try-config

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
 sudo ln -sf /home/box/web/etc/wsgi.conf /etc/gunicorn.d/wsgi-test
 sudo /etc/init.d/gunicorn restart
