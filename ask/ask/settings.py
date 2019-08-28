22abb44ffa6dfb8b5f533b630f673d838836cea7
psybrat <georgyvik@gmail.com>
2019-08-28 21:34:53 +0300

    Lesson 2.3

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -77,8 +77,9 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.path.join(BASE_DIR, 'stepic_web'),
+        'USER': 'box',
     }
 }
 
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,28 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField(default='')
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name='likes_set')
+
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
+class Answer(models.Model):
+    text = models.TextField(default='')
+    added_at = models.DateField(auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL())
